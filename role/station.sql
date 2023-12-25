780ff4ecd809bceb46055ad65b7f59a8320b8844
keerthi2402 <114648315+keerthi2402@users.noreply.github.com>
2023-12-25 09:27:38 +0530

    Create station.sql

---
+++ role/station.sql
@@ -0,0 +1,18 @@
+-- Add railway stations using the add_railway_station procedure
+-- Syntax 1: Named parameters
+CALL add_railway_station(
+    in_name  => 'Bangalore_RS',
+    in_city  => 'Bangalore',
+    in_state => 'KA'
+);
+
+-- Syntax 2: Positional parameters
+CALL add_railway_station('Chennai_RS', 'Chennai', 'TN');
+CALL add_railway_station('Delhi_RS', 'Delhi', 'DL');
+CALL add_railway_station('Gujarat_RS', 'Gandhinagar', 'GJ');
+CALL add_railway_station('Indore_RS', 'Indore', 'MP');
+CALL add_railway_station('Jaipur_RS', 'Jaipur', 'RJ');
+CALL add_railway_station('Kolkata_RS', 'Kolkata', 'WB');
+CALL add_railway_station('Lucknow_RS', 'Lucknow', 'UP');
+CALL add_railway_station('Mumbai_RS', 'Mumbai', 'MH');
+CALL add_railway_station('Palakkad_RS', 'Palakkad', 'KL');
