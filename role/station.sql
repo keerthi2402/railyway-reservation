fb2640a2e73df68015630d8a2b20ce60ca674267
keerthi2402 <114648315+keerthi2402@users.noreply.github.com>
2023-12-25 09:31:12 +0530

    Update station.sql

--- role/station.sql
+++ role/station.sql
@@ -1,18 +1,16 @@
--- Add railway stations using the add_railway_station procedure
--- Syntax 1: Named parameters
+-- Add or update railway station data with dummy values
 CALL add_railway_station(
-    in_name  => 'Bangalore_RS',
-    in_city  => 'Bangalore',
-    in_state => 'KA'
+    in_name  => 'Chennai_Central',
+    in_city  => 'Chennai',
+    in_state => 'Tamil Nadu'
 );
 
--- Syntax 2: Positional parameters
-CALL add_railway_station('Chennai_RS', 'Chennai', 'TN');
-CALL add_railway_station('Delhi_RS', 'Delhi', 'DL');
-CALL add_railway_station('Gujarat_RS', 'Gandhinagar', 'GJ');
-CALL add_railway_station('Indore_RS', 'Indore', 'MP');
-CALL add_railway_station('Jaipur_RS', 'Jaipur', 'RJ');
-CALL add_railway_station('Kolkata_RS', 'Kolkata', 'WB');
-CALL add_railway_station('Lucknow_RS', 'Lucknow', 'UP');
-CALL add_railway_station('Mumbai_RS', 'Mumbai', 'MH');
-CALL add_railway_station('Palakkad_RS', 'Palakkad', 'KL');
+CALL add_railway_station('Jodhpur_Junction', 'Jodhpur', 'Rajasthan');
+CALL add_railway_station('Ahmedabad_Station', 'Ahmedabad', 'Gujarat');
+CALL add_railway_station('Hyderabad_Junction', 'Hyderabad', 'Telangana');
+CALL add_railway_station('Kochi_Station', 'Kochi', 'Kerala');
+CALL add_railway_station('Pune_Junction', 'Pune', 'Maharashtra');
+CALL add_railway_station('Bhubaneswar_Station', 'Bhubaneswar', 'Odisha');
+CALL add_railway_station('Amritsar_Junction', 'Amritsar', 'Punjab');
+CALL add_railway_station('Guwahati_Station', 'Guwahati', 'Assam');
+CALL add_railway_station('Udaipur_Junction', 'Udaipur', 'Rajasthan');
